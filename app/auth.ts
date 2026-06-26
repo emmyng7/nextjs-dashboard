d021b1d4f5a680c87954c10a03134386a7f31f33
Emmanuel Ngene <emmyng7@gmail.com>
2026-06-26 14:55:17 +0100

    Fix auth API folder name

--- app/auth.ts
+++ app/auth.ts
@@ -20,7 +20,7 @@ async function getUser(email: string): Promise<User | undefined> {
   }
 }
 
-export const { auth, signIn, signOut } = NextAuth({
+export const { auth, handlers, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
